d781c5d8a73017d69a7ead939800c64f7ff50382
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-06-24 17:52:20 -0400

    more basic react setup

--- mern-todo-fe/src/App.js
+++ mern-todo-fe/src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Link } from "react-router-dom";
+import TodosList from "./Components/TodosList";
+import EditTodo from "./Components/EditTodo";
+import CreateTodo from "./Components/CreateTodo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <nav className="navbar nabar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          MERN-Stack Todo App
+        </Link>
+        <div className="navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link className="nav-link" to="/create">
+                create todo
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link className="nav-link" to="/">
+                home
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <Route path="/" exact component={TodosList} />
+      <Route path="/edit/:id" component={EditTodo} />
+      <Route path="/create" component={CreateTodo} />
     </div>
   );
 }
---
+++ mern-todo-fe/src/Components/CreateTodo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CreateTodo() {
+  return (
+    <div>
+      <p>create todo component</p>
+    </div>
+  );
+}
