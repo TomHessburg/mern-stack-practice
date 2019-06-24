f3d80d800d2df46e811f5db892892277b1e6ab6b
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-06-24 18:42:24 -0400

    basic react fe complete, moving to mongo backend

--- mern-todo-fe/src/Components/CreateTodo.js
+++ mern-todo-fe/src/Components/CreateTodo.js
@@ -1,9 +1,71 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function CreateTodo() {
-  return (
-    <div>
-      <p>create todo component</p>
-    </div>
-  );
+export default class CreateTodo extends Component {
+  state = {
+    todo_description: "",
+    todo_responsibility: "",
+    todo_priority: "",
+    todo_completed: false
+  };
+
+  handelChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  handelSubmit = e => {
+    e.preventDefault();
+
+    console.log(this.state);
+
+    this.setState({
+      todo_description: "",
+      todo_responsibility: "",
+      todo_priority: "",
+      todo_completed: false
+    });
+  };
+
+  render() {
+    return (
+      <div style={{ marginTop: 20 }}>
+        <h3>Create New Todo</h3>
+        <form onSubmit={this.handelSubmit}>
+          <div className="form-group">
+            <label>Description:</label>
+            <input
+              type="text"
+              className="form-control"
+              name="todo_description"
+              value={this.state.todo_description}
+              onChange={e => this.handelChange(e)}
+            />
+
+            <label>Responsible:</label>
+            <input
+              type="text"
+              className="form-control"
+              name="todo_responsibility"
+              value={this.state.todo_responsibility}
+              onChange={e => this.handelChange(e)}
+            />
+
+            <label>Priority:</label>
+            <input
+              type="text"
+              className="form-control"
+              name="todo_priority"
+              value={this.state.todo_priority}
+              onChange={e => this.handelChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-primary" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
